cfa2b5c08af79ea8c2f7d9a289b9c514dbbc6eb5
lia0519 <lita050519@naver.com>
2024-10-07 16:52:17 +0900

    Update 10808.cpp

--- 2024-2/Basic/lia0519/array/10808.cpp
+++ 2024-2/Basic/lia0519/array/10808.cpp
@@ -1,1 +1,18 @@
-code
+#include <iostream>
+using namespace std;
+
+int main() {
+	string c;
+	cin >> c;
+	
+	for (char a = 'a'; a <= 'z'; a++) {
+		int cnt = 0;
+		for (auto s : c) {
+			if (a == s) {
+				cnt++;
+			}
+		}
+		cout << cnt << " ";
+	}
+	return 0;
+}
